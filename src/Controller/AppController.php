b25abe8c39f2d1f00b868a395494894b29e98b8e
Kilian Schuster <ks@scherer-software.de>
2016-04-08 19:53:44 +0200

    Added shared versions method

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,8 +3,43 @@
 namespace CakeApiBaselayer\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Core\Configure;
+use CkTools\Lib\ApiReturnCode;
 
 class AppController extends BaseController
 {
+    /**
+     * Initialization Hook.
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('CkTools.Api');
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function beforeFilter(\Cake\Event\Event $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Api->setup();
+        $this->Api->RequestHandler->renderAs($this->Api->_registry->getController(), 'json');
+    }
+
+    /**
+     * Version getter
+     *
+     * @return string
+     */
+    public function version()
+    {
+        $configId = str_replace('/', '.', $this->request->param('plugin'));
+        $versionInfo = Configure::read($configId . '.version_info');
+        return $this->Api->response(ApiReturnCode::SUCCESS, $versionInfo);
+    }
 }
