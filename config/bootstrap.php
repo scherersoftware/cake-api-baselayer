246920c2eb44c2e137c0d064ff72e54f7fb31ba9
Felix Kempf <fk@scherer-software.de>
2016-07-14 22:28:57 +0200

    change exception renderer for api without breaking sentry monitoring

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
+use Cake\Core\Configure;
+
 if (substr(env('REQUEST_URI'), 0, 5) === '/api/') {
-    $errorHandler = new \CakeApiBaselayer\Error\ApiErrorHandler([
-        'exceptionRenderer' => '\CakeApiBaselayer\Error\ApiExceptionRenderer'
-    ]);
-    $errorHandler->register();
+    Configure::write('Error.exceptionRenderer', '\CkTools\Error\ApiExceptionRenderer');
 }
