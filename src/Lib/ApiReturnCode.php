4ae07b7ea2ba35d70cb142809d27e40aa3dbfedf
Kilian Schuster <ks@scherer-software.de>
2016-08-30 14:44:54 +0200

    Fixed HTTP Status codes

--- src/Lib/ApiReturnCode.php
+++ src/Lib/ApiReturnCode.php
@@ -8,6 +8,7 @@ class ApiReturnCode
     const INVALID_PARAMS = 'invalid_params';
     const INVALID_CREDENTIALS = 'invalid_credentials';
     const NOT_AUTHORIZED = 'not_authorized';
+    const FORBIDDEN = 'forbidden';
     const VALIDATION_FAILED = 'validation_failed';
     const NOT_FOUND = 'not_found';
     const INTERNAL_ERROR = 'internal_error';
@@ -21,10 +22,11 @@ class ApiReturnCode
     {
         return [
             self::SUCCESS => 200,
-            self::NOT_AUTHENTICATED => 403,
+            self::NOT_AUTHENTICATED => 401,
             self::INVALID_CREDENTIALS => 401,
             self::INVALID_PARAMS => 400,
             self::NOT_AUTHORIZED => 403,
+            self::FORBIDDEN => 403,
             self::NOT_FOUND => 404,
             self::VALIDATION_FAILED => 400,
             self::INTERNAL_ERROR => 500
