4f0ac410607e161332aad4ac0c81b638f2a3c14c
Wolfgang Braun <wolfgang-braun@users.noreply.github.com>
2018-03-09 17:52:34 +0100

    Merge request (#6)
    Improve api setup and redirect logic

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -27,9 +27,7 @@ class AppController extends BaseController
     public function beforeFilter(\Cake\Event\Event $event)
     {
         parent::beforeFilter($event);
-
         $this->Api->setup();
-        $this->Api->RequestHandler->renderAs($this->Api->_registry->getController(), 'json');
     }
 
     /**
@@ -37,10 +35,11 @@ class AppController extends BaseController
      */
     public function redirect($url, $status = null)
     {
-        if (Router::normalize($this->Auth->config('loginAction')) == Router::normalize($url)) {
+        if (strpos(Router::normalize($url), Router::normalize($this->Auth->config('loginAction'))) === 0) {
             return $this->Api->response(ApiReturnCode::NOT_AUTHENTICATED);
         }
-        return parent::redirect($url, $status);
+
+        return parent::redirect($url, $status);     
     }
 
     /**
@@ -52,6 +51,7 @@ class AppController extends BaseController
     {
         $configId = str_replace('/', '.', $this->request->param('plugin'));
         $versionInfo = Configure::read($configId . '.version_info');
+
         return $this->Api->response(ApiReturnCode::SUCCESS, $versionInfo);
     }
 }
